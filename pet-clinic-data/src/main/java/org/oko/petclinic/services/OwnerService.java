8694ebb55cf30872129f470b74e380333298b953
Oleksandr Kondratenko <sashko.kondratenko@gmail.com>
2020-05-10 23:00:23 +0300

    refactoring to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/org/oko/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/org/oko/petclinic/services/OwnerService.java
@@ -2,15 +2,12 @@ package org.oko.petclinic.services;
 
 import org.oko.petclinic.model.Owner;
 
-import java.util.Set;
-
 /**
  * Created by 8e3Yn4uK on 29.04.2020.
  */
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
+
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
+
 }
--- pet-clinic-data/src/main/java/org/oko/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/org/oko/petclinic/services/VetService.java
@@ -3,14 +3,10 @@ package org.oko.petclinic.services;
 
 import org.oko.petclinic.model.Vet;
 
-import java.util.Set;
-
 /**
  * Created by 8e3Yn4uK on 29.04.2020.
  */
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
+
 }
